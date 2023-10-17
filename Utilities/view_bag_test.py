f49bf5ae228d09af38d4558a0050648bfd6f7d89
DaniDoro <daniel.d@outlook.dk>
2023-10-17 11:34:39 +0200

    Added script to convert image sequence and imu as csv to bagfile

--- Utilities/view_bag_test.py
+++ Utilities/view_bag_test.py
@@ -1,20 +1,20 @@
 from rosbag import Bag
+from bagpy import bagreader
+import os
 
-path = "../svo_ws/src/bagfiles/AQUALOC/archaeo_sequence_5.bag"
+path = "gopro_test.bag"
 
-bag = Bag(path, "r")
+##### USING rosbag #####
+# bag = Bag(path, "r")
+# get the list of topics
+# topics = bag.get_type_and_topic_info()[1].keys() # All the topics info
+# print("\nAvailable topics in bag file are:{}".format(topics))
+
+##### USING bagpy #####
+bag = bagreader(path)
+
+# Remove ".bag" part
+os.rmdir(path[:-4])
 
 # get the list of topics
-topics = bag.get_type_and_topic_info()[1].keys() # All the topics info
-print("\nAvailable topics in bag file are:{}".format(topics))
-
-# get all the messages of type velocity
-# velmsgs   = b.vel_data()
-# veldf = pd.read_csv(velmsgs[0])
-# plt.plot(veldf['Time'], veldf['linear.x'])
-
-# # quickly plot velocities
-# b.plot_vel(save_fig=True)
-
-# # you can animate a timeseries data
-# bagpy.animate_timeseries(veldf['Time'], veldf['linear.x'], title='Velocity Timeseries Plot')
+print(bag.topic_table)
