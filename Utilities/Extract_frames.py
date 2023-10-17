d50c57252233a8952859b253e3aee0dd2a905449
bruno10mds <bruno10mds@gmail.com>
2023-10-17 10:44:35 +0200

    Added progress bar and frame skipping

--- Utilities/Extract_frames.py
+++ Utilities/Extract_frames.py
@@ -1,8 +1,11 @@
 import cv2
 import os
+from tqdm import tqdm
 
 
-def save_all_frames(video_path, dir_path, basename, ext='jpg'):
+
+def save_all_frames(video_path, dir_path, basename, ext='jpg', frame_rm = 3):
+    
     cap = cv2.VideoCapture(video_path)
 
     if not cap.isOpened():
@@ -15,14 +18,18 @@ def save_all_frames(video_path, dir_path, basename, ext='jpg'):
 
     n = 0
 
-    while True:
+    for f in tqdm(range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)/frame_rm))):
         ret, frame = cap.read()
         if ret:
-            cv2.imwrite('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext), frame)
+            if n%frame_rm == 0:
+                frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cv2.imwrite('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext), frame_gray)
             n += 1
+            
+            
         else:
             return
 
-save_all_frames('data/temp/FeatureChange.mp4', 'data/temp/result', 'sample_video_img')
+#save_all_frames('data/temp/FeatureChange.mp4', 'data/temp/result', 'sample_video_img')
 
-#save_all_frames('data/temp/sample_video.mp4', 'data/temp/result_png', 'sample_video_img', 'png')
+save_all_frames('D:/Rob7/21-09-2023_pilot_day/GoPro1/GX040003.MP4', 'D:/Rob7/21-09-2023_pilot_day/GoPro1/data/temp/result_png', 'sample_video_img', 'png')
