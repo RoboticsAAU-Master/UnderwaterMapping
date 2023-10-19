0f65ce8d807346d2b8a730f6269302d4661773fe
jbla20 <72883493+jbla20@users.noreply.github.com>
2023-10-19 16:11:31 +0200

    Added synchronisation of GoPro audio

---
+++ Utilities/GoPro-synchronise-audio/sync_audio.py
@@ -0,0 +1,19 @@
+from syncstart import file_offset
+
+def get_offset(video1, video2, output_file):
+    args = {'in1': video1,
+            'in2': video2,
+            'take': 20} # Seconds of videos to keep (20 is default)
+    
+    file_ahead, offset = file_offset(**args)
+    
+    # Write offset to file
+    with open(output_file, "w") as file:
+        # Write the value to the file
+        file.write(file_ahead + f" is ahead by: {offset} [s]\n")
+
+if __name__ == "__main__":
+    # Input: path_video1  path_video2  output_file
+    get_offset("Utilities/GoPro-synchronise-audio/Output/GoPro1_Clap.MP4",
+               "Utilities/GoPro-synchronise-audio/Output/GoPro2_Clap.MP4",
+               "Utilities/GoPro-synchronise-audio/Output/Kridtgraven-20-09-23.txt")
