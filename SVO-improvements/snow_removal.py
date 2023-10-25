cb975feaecfba1fcbe4308225037a6fd8df32318
Daniel D <daniel.d@outlook.dk>
2023-10-25 10:19:50 +0200

    Modified marine snow removal
    It's a bit better than before, but still not optimal.

--- SVO-improvements/snow_removal.py
+++ SVO-improvements/snow_removal.py
@@ -1,77 +1,142 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
-from guided_filter import _gf_gray 
+from guided_filter import _gf_gray
+from time import time
+
 
 # Function to get index for circular array
-def wrap_index(index : int) -> int:
+def wrap_index(index: int) -> int:
     return index % 3
 
-# cap = cv.VideoCapture("marine_snow_large.MP4")
-cap = cv.VideoCapture("C4_GX040003.MP4")
+
+cap = cv.VideoCapture("marine_snow.MP4")
+# cap = cv.VideoCapture("C4_GX040003.MP4")
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
-    
+
 NUM_FRAMES = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
 
 frame_counter = 0
 
 images = []
-COEFFS = np.array([0.114, 0.587, 0.299]) # Coefficients for Y-channel (BGR)
-idx = 1 # Index for accesing images
-r = 8 # Radius of mean filter
-eps = 0.05 # Regularisation parameter that controls degree of smoothness 
-s = 4 # Subsampling ratio
+COEFFS = np.array([0.114, 0.587, 0.299])  # Coefficients for Y-channel (BGR)
+idx = 1  # Index for accesing images
+r = 5  # Radius of mean filter
+eps = 0.05  # Regularisation parameter that controls degree of smoothness
+s = 4  # Subsampling ratio
 
-cv.namedWindow("Output", cv.WINDOW_NORMAL)    # Create window with freedom of dimensions
-cv.namedWindow("Original", cv.WINDOW_NORMAL)    # Create window with freedom of dimensions
+# Create window with freedom of dimensions
+cv.namedWindow("Output", cv.WINDOW_NORMAL)
+# Create window with freedom of dimensions
+cv.namedWindow("Original", cv.WINDOW_NORMAL)
+
+start_time = time()
+# Kernel for tophat morphology
+kernel1 = np.ones((3, 3), np.uint8)
+w = 50  # Kernel size for density map
+kernel2 = np.ones((w, w)) / (w**2)
+# Kernel for open/dilation operation of feature mask
+kernel3 = cv.getStructuringElement(cv.MORPH_RECT, (w, w))
 
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame = cv.resize(frame, (0,0), fx=0.5, fy=0.5)
+    # frame = cv.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    # frame = cv.resize(frame, (0, 0), fx=1, fy=1)
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    
+
     frame_counter += 1
     # Converting input image to gray with weights
-    Y = cv.transform(frame, COEFFS.reshape((1,3)))
-    Y_blurred = cv.blur(Y, (2*r+1,2*r+1), cv.BORDER_REPLICATE)
-    Y_lp = _gf_gray(Y, Y, r, eps, s)
-    Y_lp = Y_lp.astype(np.uint8)
-    #Y_hp = Y - Y_lp
+    Y = cv.transform(frame, COEFFS.reshape((1, 3)))
+    # Y_blurred = cv.GaussianBlur(Y, (2 * r + 1, 2 * r + 1), cv.BORDER_REPLICATE)
+    Y_blurred = cv.blur(Y, (2 * r + 1, 2 * r + 1), cv.BORDER_REPLICATE)
+
+    # Y_lp = _gf_gray(Y, Y, r, eps, s)
+    # Y_lp = Y_lp.astype(np.uint8)
+    # Y_hp = Y - Y_lp
     # Y_hp = cv.subtract(Y, Y_lp)
     Y_hp = cv.subtract(Y, Y_blurred)
-    
+
     if len(images) < 3:
         images.append(Y_hp)
-        continue
-    
-    prev_img = images[wrap_index(idx-1)]
+        if len(images) < 3:
+            disp_frame = np.zeros_like(frame)
+            continue
+
+    prev_img = images[wrap_index(idx - 1)]
     curr_img = images[wrap_index(idx)]
-    next_img = images[wrap_index(idx+1)]
-    
+    next_img = images[wrap_index(idx + 1)]
+
+    # Keep moving particles using temporal information
     P = cv.subtract(curr_img, np.minimum(prev_img, next_img))
-    
-    images[wrap_index(idx-1)] = Y_hp
-    
+    # Threshold P to get binary image
+    (_, P) = cv.threshold(P, 1, 255, cv.THRESH_BINARY)
+    # Use tophat morphology to keep all small particles
+    P_particles = cv.morphologyEx(P, cv.MORPH_TOPHAT, kernel1)
+    # Subtract from original P to keep larger particles
+    P = cv.subtract(P, P_particles)
+
+    # Compute density map
+    P_density = cv.filter2D(P, -1, kernel2, borderType=cv.BORDER_REPLICATE)
+    # Threshold to only keep dense regions corresponding to features, i.e. locate areas of many features
+    _, P_density = cv.threshold(P_density, 6, 255, cv.THRESH_BINARY)
+    # Perform opening operation to remove noise
+    P_density = cv.morphologyEx(P_density, cv.MORPH_OPEN, kernel3)
+    # Perform closing to remove holes in blobs
+    P_density = cv.morphologyEx(P_density, cv.MORPH_DILATE, kernel3)
+    # Isolate snow by subtracting feature mask
+    snow_mask = cv.subtract(P, P_density)
+
+    # Update the future image
+    images[wrap_index(idx - 1)] = Y_hp
+
     idx += 1
-    
-    # Display the resulting frame
-    #cv.imshow('Output', cv.medianBlur(Y_hp, 3))
-    cv.imshow('Output', np.multiply(P,10))
-    cv.imshow('Original', frame)
-    if cv.waitKey(1) == ord('q'):
+
+    # Create an RGB image so that it can be overlayed with the frame
+    P_mask_rgb = cv.merge(
+        [np.zeros_like(snow_mask), np.zeros_like(snow_mask), snow_mask]
+    )
+
+    # Overlay snow mask with original image
+    P_overlay = cv.addWeighted(disp_frame, 1, P_mask_rgb, 0.5, 0)
+
+    P_overlay = cv.putText(
+        P_overlay,
+        f"FPS: {(1 / (time() - start_time)):.2f}",
+        (0, 100),
+        cv.FONT_HERSHEY_SIMPLEX,
+        2,
+        (0, 0, 0),
+        2,
+        cv.LINE_AA,
+    )
+
+    # Concatenate the two images horizontally
+    # concatenated_image = cv.hconcat(
+    #     [P_overlay, cv.merge([snow_mask, snow_mask, snow_mask])]
+    # )
+    concatenaed_image = cv.hconcat(
+        [P_overlay, cv.merge([snow_mask, snow_mask, snow_mask])]
+    )
+    # cv.imshow("P", P_density)
+    cv.imshow("Output", concatenaed_image)
+    cv.imshow("Original", disp_frame)
+    if cv.waitKey(1) == ord("q"):
         break
-    
-    #If the last frame is reached, reset the capture and the frame_counter
+
+    start_time = time()
+    disp_frame = frame
+
+    # If the last frame is reached, reset the capture and the frame_counter
     if frame_counter == NUM_FRAMES - 1:
-        frame_counter = 0 #Or whatever as long as it is the same as next line
+        frame_counter = 0  # Or whatever as long as it is the same as next line
         cap.set(cv.CAP_PROP_POS_FRAMES, 0)
-        
+
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
--- Utilities/GoPro-data-extraction/extract_frames.py
+++ Utilities/GoPro-data-extraction/extract_frames.py
@@ -4,7 +4,6 @@ from tqdm import tqdm
 
 
 def save_all_frames(video_path, dir_path, basename, ext, frame_keep, downscale):
-    
     cap = cv2.VideoCapture(video_path)
 
     if not cap.isOpened():
@@ -14,7 +13,7 @@ def save_all_frames(video_path, dir_path, basename, ext, frame_keep, downscale):
     base_path = os.path.join(dir_path, basename)
 
     digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
-    
+
     n = 0
     for f in tqdm(range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))):
         ret, frame = cap.read()
@@ -22,19 +21,23 @@ def save_all_frames(video_path, dir_path, basename, ext, frame_keep, downscale):
             if (n % frame_keep) == 0:
                 frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 # Rescale image
-                frame_gray = cv2.resize(frame_gray, (0,0), fx=downscale, fy=downscale)
-                cv2.imwrite('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext), frame_gray)
+                frame_gray = cv2.resize(frame_gray, (0, 0), fx=downscale, fy=downscale)
+                cv2.imwrite(
+                    "{}_{}.{}".format(base_path, str(n).zfill(digit), ext), frame_gray
+                )
             n += 1
-            
+
         else:
             return
-        
+
 
 if __name__ == "__main__":
     # Input: path_to_video  output_folder  base_image_name  extension
-    save_all_frames('C4_GX040003.MP4', 
-                    'Utilities/GoPro-data-extraction/Output/C4_GX040003/Images', 
-                    'sample_video_img', 
-                    'png', 
-                    1,
-                    0.5)
+    save_all_frames(
+        "C4_GX040003.MP4",
+        "Utilities/GoPro-data-extraction/Output/C4_GX040003/Images",
+        "sample_video_img",
+        "png",
+        1,
+        0.5,
+    )
