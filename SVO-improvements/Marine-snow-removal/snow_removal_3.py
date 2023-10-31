ebd78ab3c1811ea671494d8f2ba95caec8672050
jbla20 <72883493+jbla20@users.noreply.github.com>
2023-10-31 08:26:12 +0100

    Added blob analysis

--- SVO-improvements/Marine-snow-removal/snow_removal_3.py
+++ SVO-improvements/Marine-snow-removal/snow_removal_3.py
@@ -37,7 +37,7 @@ kernel2 = np.ones((w, w)) / (w**2)
 # Kernel for open/dilation operation of feature mask
 kernel3 = cv.getStructuringElement(cv.MORPH_RECT, (w, w))
 # Threshold for dense regions
-th_dense = 6
+th_dense = 4
 # Number of superpixels (components)
 num_components = 100
 # Compactness parameter for superpixels
@@ -82,10 +82,26 @@ while True:
     # Compute binary density map
     density_map = density_segmentation(rectified, kernel2, th_dense, kernel3)
     # Remove dense areas (features)
-    snow_mask = cv.subtract(cv.multiply(rectified, 255), cv.multiply(density_map, 255))
+    snow_mask = cv.subtract(rectified, density_map)
+
+    # Blob analysis
+    num_labels, blobs, stats, centroids = cv.connectedComponentsWithStats(snow_mask)
+    blob_mask = np.zeros_like(snow_mask)
+    # Compute the area for all labeled components in one go
+    blob_areas = stats[1:, cv.CC_STAT_AREA]
+    # Create an array of labels to keep based on the area threshold
+    remove_labels = (
+        np.where(blob_areas < 15)[0] + 1
+    )  # Adding 1 to account for 0-based indexing
+    # Create a mask of labels to keep
+    remove_mask = np.isin(blobs, remove_labels)
+    # Apply the keep_mask to the original image
+    blob_mask[remove_mask] = 255
+    snow_mask2 = cv.subtract(snow_mask, blob_mask)
 
     # Overlay snow mask with original image
-    P_overlay = cv.addWeighted(Y, 1, snow_mask, 0.5, 0)
+    P_mask_rgb = cv.merge([snow_mask2, snow_mask2, snow_mask2])
+    P_overlay = cv.addWeighted(frame, 1, P_mask_rgb, 1, 0)
 
     P_overlay = cv.putText(
         P_overlay,
@@ -98,12 +114,20 @@ while True:
         cv.LINE_AA,
     )
 
-    concatenate_image = cv.hconcat([P_overlay, snow_mask])
+    concatenate_image = cv.hconcat([P_overlay, P_mask_rgb])
 
     cv.imshow("Output", concatenate_image)
 
     if cv.waitKey(1) == ord("q"):
         break
+
+    # if idx == 508:
+    #     cv.imwrite("marine_snow_rgb3.png", frame)
+    #     cv.imwrite("marine_snow_monochrome3.png", Y)
+    #     cv.imwrite("marine_snow_rectified3.png", rectified)
+    #     cv.imwrite("marine_snow_density3.png", snow_mask)
+    #     cv.imwrite("marine_snow_blob3.png", snow_mask2)
+    #     cv.imwrite("marine_snow_overlay3.png", P_overlay)
 
     start_time = time()
     # disp_frame = frame
