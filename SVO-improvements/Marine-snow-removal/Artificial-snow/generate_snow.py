e480436af5aaa01682809c668c63407b59399d0d
Daniel D <daniel.d@outlook.dk>
2023-11-14 15:40:58 +0100

    Fixed marine snow dilation and added context-aware overlay
    Fixed issue where mask had dilated marine snow due to bluring, and modified code so that it does not add marine snow over regions where there is high variance, or very low saturation and high value
    
    Co-Authored-By: jbla20 <72883493+jbla20@users.noreply.github.com>

--- SVO-improvements/Marine-snow-removal/Artificial-snow/generate_snow.py
+++ SVO-improvements/Marine-snow-removal/Artificial-snow/generate_snow.py
@@ -111,6 +111,16 @@ def generate_snow(ref_image, mask=False):
     hsv = cv.cvtColor(ref_image, cv.COLOR_BGR2HSV)
 
     background_val = get_background(hsv, 2)
+    median_blur = cv.medianBlur(hsv, 5)
+
+    hsv_32 = np.float32(hsv)
+    # Calculate mean of image
+    mu = cv.blur(hsv_32, (7, 7), borderType=cv.BORDER_CONSTANT)
+    # Calculate mean of image squared
+    mu2 = cv.blur(np.multiply(hsv_32, hsv_32), (7, 7), borderType=cv.BORDER_CONSTANT)
+    # Calculate variance of image
+    test = np.subtract(mu2, np.multiply(mu, mu))
+    sigma = np.sqrt(np.subtract(mu2, np.multiply(mu, mu)))
 
     # Calculate the number of snow particles
     num_range = (0, 100)
@@ -118,6 +128,12 @@ def generate_snow(ref_image, mask=False):
 
     # Generate Halton sequence
     samples_coords = get_random_samples(ref_image, num_snow, method="random")
+
+    # Concatenate images
+    test = cv.resize(hsv, (0, 0), fx=0.4, fy=0.4)
+    hsv_concat = cv.hconcat([test[:, :, 0], test[:, :, 1], test[:, :, 2]])
+    cv.imshow("HSV", hsv_concat)
+    cv.waitKey(0)
 
     # Shape and size of snow particles
     snow_mask = np.zeros_like(hsv)
@@ -128,6 +144,20 @@ def generate_snow(ref_image, mask=False):
         val = np.array(
             [hsv[y, x, 0], np.random.randint(0, 80), np.random.randint(50, 255)]
         )
+
+        # val[2] < 1.2 * median_blur[y, x, 1] and val[2] < 1.2 * median_blur[y, x, 2]
+        if (median_blur[y, x, 1] < 50 and median_blur[y, x, 2] > 240) or (
+            (sigma[y, x, 1] > 5 or sigma[y, x, 2] > 5)
+        ):
+            s_thresh = (hsv[:, :, 1] < 50).astype(np.uint8)
+            v_thresh = (hsv[:, :, 2] > 240).astype(np.uint8)
+            temp = cv.bitwise_and(s_thresh, v_thresh)
+            temp = cv.bitwise_or(temp, (sigma[:, :, 1] > 5).astype(np.uint8))
+            temp = cv.bitwise_or(temp, (sigma[:, :, 2] > 5).astype(np.uint8))
+            cv.imshow("temp", temp * 255)
+            cv.waitKey(0)
+            continue
+
         snow_mask[y, x, :] = val
 
         # Get direction along which we stretch the marine snow particle
@@ -164,6 +194,8 @@ def generate_snow(ref_image, mask=False):
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3 + 2 * (3 // speed), 3))
     snow_mask = cv.dilate(snow_mask, kernel, iterations=1)
 
+    _, non_zero_mask = cv.threshold(snow_mask[:, :, 2], 0, 255, cv.THRESH_BINARY)
+
     # Blur the pixels with a Gaussian kernel proportional to the speed (motion blur)
     snow_mask = cv.GaussianBlur(
         snow_mask, (3 + 2 * (speed // 2), 3 + 2 * (speed // 2)), 0
@@ -176,7 +208,6 @@ def generate_snow(ref_image, mask=False):
     overlay_percentage = (background_val / 255) * (0.4 - 0.1) + 0.1
     out_image = cv.addWeighted(ref_image, 1, snow_mask, overlay_percentage, 0)
 
-    _, non_zero_mask = cv.threshold(snow_mask[:, :, 2], 0, 255, cv.THRESH_BINARY)
     # non_zero_mask = snow_mask[:, :, 2] != 0
     # out_image = ref_image.copy()
     # out_image[non_zero_mask] = snow_mask[non_zero_mask]
@@ -192,12 +223,15 @@ if __name__ == "__main__":
     scale = 0.5
 
     # Read image
-    img = cv.imread("Artificial-snow/Input/original_image_1.png", cv.IMREAD_COLOR)
+    img = cv.imread(
+        r"E:\data\Input\original_image_4.png",
+        cv.IMREAD_COLOR,
+    )
     img_resized = cv.resize(img, (0, 0), fx=scale, fy=scale)
 
     # Resize images
     while True:
-        img_snow = generate_snow(img)
+        img_snow, mask = generate_snow(img, mask=True)
         img_snow_resized = cv.resize(img_snow, (0, 0), fx=scale, fy=scale)
 
         # Concatenate images
@@ -205,6 +239,7 @@ if __name__ == "__main__":
 
         # Show images
         cv.imshow("Original vs Snow", concatenated)
+        cv.imshow("Mask", mask)
         if cv.waitKey(0) == ord("q"):
             break
 
