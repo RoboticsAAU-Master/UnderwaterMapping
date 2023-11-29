df430768344ec5da658bfa74c0c438d0991bbafd
jbla20 <72883493+jbla20@users.noreply.github.com>
2023-11-29 09:35:08 +0100

    Added manual synchronisation

--- Utilities/Bag-conversion/gopro_to_bag.py
+++ Utilities/Bag-conversion/gopro_to_bag.py
@@ -2,7 +2,8 @@ import time, sys, os
 from ros import rosbag
 import roslib
 import rospy
-roslib.load_manifest('sensor_msgs')
+
+roslib.load_manifest("sensor_msgs")
 from sensor_msgs.msg import Image, Imu
 from cv_bridge import CvBridge
 import pandas as pd
@@ -12,43 +13,46 @@ from PIL import ImageFile
 
 import cv2
 
-CAM_SAMPLE_RATE = 60.0 # Hz
-IMU_SAMPLE_RATE = 197.720721 # Hz
+CAM_SAMPLE_RATE = 60.0  # Hz
+IMU_SAMPLE_RATE = 197.720721  # Hz
+TIME_START = 0.0
+TIME_END = 1000.0
 
 STANDARD_TO_TOPIC = {
-    'euroc_mono' :   ['/cam0/image_raw','/imu0'],
-    'euroc_stereo' : ['/cam0/image_raw','/cam1/image_raw','/imu0'],
-    'fla_mono' :     ['/sync/cam0/image_raw','/sync/imu/imu'],
-    'fla_stereo' :   ['/sync/cam0/image_raw','/sync/cam1/image_raw','/sync/imu/imu']
+    "euroc_mono": ["/cam0/image_raw", "/imu0"],
+    "euroc_stereo": ["/cam0/image_raw", "/cam1/image_raw", "/imu0"],
+    "fla_mono": ["/sync/cam0/image_raw", "/sync/imu/imu"],
+    "fla_stereo": ["/sync/cam0/image_raw", "/sync/cam1/image_raw", "/sync/imu/imu"],
 }
 
+
 def GetFilesFromDir(dir):
-    '''Generates a list of files from the directory'''
-    print( "Searching directory %s" % dir )
+    """Generates a list of files from the directory"""
+    print("Searching directory %s" % dir)
     all = []
     left_files = []
     right_files = []
     if os.path.exists(dir):
         for path, names, files in os.walk(dir):
             for f in sorted(files):
-                if os.path.splitext(f)[1] in ['.bmp', '.png', '.jpg', '.ppm']:
-                    if 'left' in f or 'left' in path:
-                        left_files.append( os.path.join( path, f ) )
-                    elif 'right' in f or 'right' in path:
-                        right_files.append( os.path.join( path, f ) )
-                    all.append( os.path.join( path, f ) )
+                if os.path.splitext(f)[1] in [".bmp", ".png", ".jpg", ".ppm"]:
+                    if "left" in f or "left" in path:
+                        left_files.append(os.path.join(path, f))
+                    elif "right" in f or "right" in path:
+                        right_files.append(os.path.join(path, f))
+                    all.append(os.path.join(path, f))
     return all, left_files, right_files
 
 
 def CreateStereoBag(left_imgs, right_imgs, bagname, standard):
-    '''Creates a bag file containing stereo image pairs'''
-    bag =rosbag.Bag(bagname, 'w')
+    """Creates a bag file containing stereo image pairs"""
+    bag = rosbag.Bag(bagname, "w")
 
     timer = 0
 
     try:
         for i in tqdm(range(len(left_imgs))):
-            #print("Adding %s" % left_imgs[i])
+            # print("Adding %s" % left_imgs[i])
             img_left = cv2.imread(left_imgs[i], cv2.IMREAD_GRAYSCALE)
             img_right = cv2.imread(right_imgs[i], cv2.IMREAD_GRAYSCALE)
 
@@ -58,7 +62,7 @@ def CreateStereoBag(left_imgs, right_imgs, bagname, standard):
 
             # Since image acquisition frequency depends on write speed, the timestamp is synthesized
             Stamp = rospy.Time.from_sec(timer)
-            timer += 1/CAM_SAMPLE_RATE
+            timer += 1 / CAM_SAMPLE_RATE
 
             img_msg_left = Image()
             img_msg_left = bridge.cv2_to_imgmsg(img_left, "mono8")
@@ -80,14 +84,14 @@ def CreateStereoBag(left_imgs, right_imgs, bagname, standard):
 
 
 def CreateMonoBag(imgs, bagname, standard):
-    '''Creates a bag file with camera images'''
-    bag =rosbag.Bag(bagname, 'w')
+    """Creates a bag file with camera images"""
+    bag = rosbag.Bag(bagname, "w")
 
     timer = 0
 
     try:
         for i in tqdm(range(len(imgs))):
-            #print("Adding %s" % imgs[i])
+            # print("Adding %s" % imgs[i])
             img = cv2.imread(imgs[i], cv2.IMREAD_GRAYSCALE)
             bridge = CvBridge()
 
@@ -95,7 +99,7 @@ def CreateMonoBag(imgs, bagname, standard):
 
             # Since image acquisition frequency depends on write speed, the timestamp is synthesized
             Stamp = rospy.Time.from_sec(timer)
-            timer += 1/CAM_SAMPLE_RATE
+            timer += 1 / CAM_SAMPLE_RATE
 
             img_msg = Image()
             img_msg = bridge.cv2_to_imgmsg(img, "mono8")
@@ -109,20 +113,20 @@ def CreateMonoBag(imgs, bagname, standard):
 
 
 def CreateBag(args):
-    '''Creates the actual bag file by successively adding images'''
+    """Creates the actual bag file by successively adding images"""
     all_imgs, left_imgs, right_imgs = GetFilesFromDir(args[0])
     if len(all_imgs) <= 0:
         print("No images found in %s" % args[0])
         exit()
 
     if len(left_imgs) > 0 and len(right_imgs) > 0:
-        if not 'stereo' in args[3]:
-            raise Exception("Stereo data provided does not match standard") 
+        if not "stereo" in args[3]:
+            raise Exception("Stereo data provided does not match standard")
         # create bagfile with stereo camera image pairs
         CreateStereoBag(left_imgs, right_imgs, args[1], args[3])
     else:
-        if not 'mono' in args[3]:
-            raise Exception("Mono data provided does not match standard") 
+        if not "mono" in args[3]:
+            raise Exception("Mono data provided does not match standard")
         # create bagfile with mono camera image stream
         CreateMonoBag(all_imgs, args[1], args[3])
 
@@ -131,38 +135,44 @@ def CreateBag(args):
         imu_data = GetImuData(args[2])
         AddImuToBag(args[1], imu_data, args[3])
 
-def GetImuData(dir : str):
-    '''Read accelerometer and gyroscope data from path'''
-    print( "Searching directory %s" % dir )
+
+def GetImuData(dir: str):
+    """Read accelerometer and gyroscope data from path"""
+    print("Searching directory %s" % dir)
 
     if os.path.exists(dir):
         for path, names, files in os.walk(dir):
             for f in files:
-                if os.path.splitext(f)[1] in ['.csv', '.ods']:
-                    if 'Accl' in f or 'Accl' in path:
-                        accl_data = pd.read_csv(os.path.join( path, f ), header=None)
-                    elif 'Gyro' in f or 'Gyro' in path:
-                        gyro_data = pd.read_csv(os.path.join( path, f ), header=None)
+                if os.path.splitext(f)[1] in [".csv", ".ods"]:
+                    if "Accl" in f or "Accl" in path:
+                        accl_data = pd.read_csv(os.path.join(path, f), header=None)
+                    elif "Gyro" in f or "Gyro" in path:
+                        gyro_data = pd.read_csv(os.path.join(path, f), header=None)
 
-    # Delete last column, corresponding to NaN. 
+    # Delete last column, corresponding to NaN.
     accl_data.drop(accl_data.columns[-1], axis=1, inplace=True)
     gyro_data.drop(gyro_data.columns[-1], axis=1, inplace=True)
-    
+
     imu_data = pd.concat([accl_data, gyro_data], axis=1)
 
     return imu_data
 
-def AddImuToBag(bagname : str, imu_data, standard):
 
-    with rosbag.Bag(bagname, 'a') as bag:
-
+def AddImuToBag(bagname: str, imu_data, standard):
+    with rosbag.Bag(bagname, "a") as bag:
         timer = 0
 
         for index, row in imu_data.iterrows():
+            if (
+                index < TIME_START * IMU_SAMPLE_RATE
+                or index > TIME_END * IMU_SAMPLE_RATE
+            ):
+                continue
+
             row = row.tolist()
             timestamp = rospy.Time.from_sec(timer)
-            timer += 1/IMU_SAMPLE_RATE
-            
+            timer += 1 / IMU_SAMPLE_RATE
+
             imu_msg = Imu()
             imu_msg.header.stamp = timestamp
             imu_msg.linear_acceleration.x = row[0]
@@ -180,9 +190,13 @@ if __name__ == "__main__":
     #     CreateBag( sys.argv[1:])
     # else:
     #     print( "Usage: SCRIPT_NAME.py imagedir bagfilename imudir data_standard")
-    
+
     # OBS: Convert input images to grayscale
-    CreateBag(args=["Utilities/GoPro-data-extraction/Output/C4/Images", 
-                    "Utilities/Bag-conversion/Output/C4.bag", 
-                    "Utilities/GoPro-data-extraction/Output/C4/Metadata",
-                    "euroc_stereo"])
+    CreateBag(
+        args=[
+            "Utilities/GoPro-data-extraction/Output/C4/Images",
+            "Utilities/Bag-conversion/Output/C4.bag",
+            "Utilities/GoPro-data-extraction/Output/C4/Metadata",
+            "euroc_stereo",
+        ]
+    )
--- Utilities/GoPro-data-extraction/extract_frames.py
+++ Utilities/GoPro-data-extraction/extract_frames.py
@@ -2,6 +2,9 @@ import cv2
 import os
 from tqdm import tqdm
 
+TIME_START = 0.0
+TIME_END = 1000.0
+
 
 def save_all_frames(
     video_path, dir_path, basename, ext, gray=True, frame_skip=0, downscale=1
@@ -17,14 +20,22 @@ def save_all_frames(
     digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
 
     n = 0
+    t = 0
     for f in tqdm(range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))):
         ret, frame = cap.read()
+
+        if t < TIME_START or t > TIME_END:
+            t += 1 / 60.0
+            continue
+
         if ret:
             if (n % (frame_skip + 1)) == 0:
                 if gray:
                     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 # Rescale image
-                frame = cv2.resize(frame, (0, 0), fx=downscale, fy=downscale)
+                if downscale != 1:
+                    frame = cv2.resize(frame, (0, 0), fx=downscale, fy=downscale)
+
                 cv2.imwrite(
                     "{}_{}.{}".format(base_path, str(n).zfill(digit), ext), frame
                 )
@@ -37,11 +48,11 @@ def save_all_frames(
 if __name__ == "__main__":
     # Input: path_to_video  output_folder  base_image_name  extension
     save_all_frames(
-        "AprilTag_L.MP4",
-        "Utilities/GoPro-data-extraction/Output/Calibration/ImagesLeft",
-        "img_left",
+        "AW_calib1_right.MP4",
+        "Utilities/GoPro-data-extraction/Output/Calibration_AW/Images",
+        "img_right",
         "png",
-        gray=False,
-        frame_skip=0,
-        downscale=1,
+        gray=True,
+        frame_skip=0,  # [0, Number of frames]
+        downscale=1,  # [0, 1]
     )
