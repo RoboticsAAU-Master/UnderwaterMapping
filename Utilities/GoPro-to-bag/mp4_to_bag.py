3c3642f95cac59c32b55677602a1b8a73faeb7de
jbla20 <72883493+jbla20@users.noreply.github.com>
2024-02-29 20:25:31 +0100

    Some minor folder path changes

--- Utilities/GoPro-to-bag/mp4_to_bag.py
+++ Utilities/GoPro-to-bag/mp4_to_bag.py
@@ -15,6 +15,7 @@ with open(base_path + 'GoPro-to-bag/times.json') as json_file:
     times = json.load(json_file)
     new_times = times.copy()
 
+processed = 0
 for video_left, video_right in zip(videos_left, videos_right):
     ### Prechecks to ensure correct video pair ###
     video_left_base = ntpath.basename(video_left)
@@ -31,8 +32,8 @@ for video_left, video_right in zip(videos_left, videos_right):
     ### Obtaining synchronisation offset between videos ###
     from sync_audio import get_offset
 
-    output_file = base_path + "GoPro-synchronise-audio/Output/offsets.txt"
-    offset = get_offset(video_left, video_right, output_file, overwrite=True)
+    output_file_offset = base_path + "GoPro-synchronise-audio/Output/offsets.txt"
+    offset = get_offset(video_left, video_right, output_file_offset, overwrite=True)
     
     new_times[video_left_base[:7]]["left_crop"] = offset
     
@@ -40,37 +41,40 @@ for video_left, video_right in zip(videos_left, videos_right):
     ### Extracting the frames from the videos ###
     from extract_frames import save_all_frames
     
-    output_folder = base_path + "GoPro-data-extraction/Output/" + video_left_base[:7] + "/Images"
-    save_all_frames(video_left, output_folder, "img_left", "png", T_START, T_END, gray=True, frame_skip=2, downscale=0.5)
-    save_all_frames(video_right, output_folder, "img_right", "png", T_START - offset, T_END - offset, gray=True, frame_skip=2, downscale=0.5)
+    output_folder_images = base_path + "GoPro-data-extraction/Output/" + video_left_base[:7] + "/Images"
+    save_all_frames(video_left, output_folder_images, "img_left", "png", T_START, T_END, gray=True, frame_skip=2, downscale=0.5)
+    save_all_frames(video_right, output_folder_images, "img_right", "png", T_START - offset, T_END - offset, gray=True, frame_skip=2, downscale=0.5)
     
     
     ### Extracting the metadata from the videos ###
-    output_folder = base_path + "GoPro-data-extraction/Output/" + video_left_base[:7] + "/Metadata"
-    subprocess.run([base_path + "GoPro-to-bag/extract_metadata.sh", video_left, output_folder])
+    output_folder_metadata = base_path + "GoPro-data-extraction/Output/" + video_left_base[:7] + "/Metadata"
+    subprocess.run([base_path + "GoPro-to-bag/extract_metadata.sh", video_left, output_folder_metadata])
 
     from determine_onset import determine_onset
     try :
-        onset_time = determine_onset(output_folder + "/outputAccl.csv")
+        onset_time = determine_onset(output_folder_metadata + "/outputAccl.csv")
         new_times[video_left_base[:7]]["left_onset"] = onset_time
     except ValueError as e:
         print(e)
-        continue
     
 
     ### Converting the images and metadata to a bag file ###
     from gopro_to_bag import CreateBag
 
+    output_file = base_path + "Bag-conversion/Output/" + video_left_base[:7] + ".bag"
     CreateBag(args=[
-        base_path + "GoPro-data-extraction/Output/1,1_0_0_10/Images",
-        base_path + "Bag-conversion/Output/1,1_0_0_10.bag",
-        base_path + "GoPro-data-extraction/Output/1,1_0_0_10/Metadata",
+        output_folder_images,
+        output_file,
+        output_folder_metadata,
         "euroc_stereo",
         T_START,
         T_END,
     ])
+    
+    processed += 1
 
-print("Conversion complete")
+print(f"Conversion complete. Processed {processed} videos")
+
 
 # Convert and write JSON object to file with new times
 with open(base_path + "GoPro-to-bag/new_times.json", "w") as outfile: 
